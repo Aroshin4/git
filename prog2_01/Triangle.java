2f6420269b875e208770451a5c4eb4e37a6508a2
Aroshin4 <166681444+Aroshin4@users.noreply.github.com>
2024-04-24 16:16:04 +0900

    Add files via upload

--- prog2_01/Triangle.java
+++ prog2_01/Triangle.java
@@ -1,34 +1,52 @@
 // 課題: 三角形クラス Triangle.java
 
 public class Triangle {
-
-private double a;
-private double b;
-private double c;
-public Triangle (double aa,double bb,double cc){
-    if((aa+bb)>cc&&(bb+cc)>aa&&(cc+aa)>bb){
-       a=aa;
-       b=bb;
-       c=cc;
-       }else{
-        return false
-       }
-}
-public Triangele(double aa,double bb){
-    if ((2*aa)>bb){
-       a=b=aa; 
-       c=bb;
-    }else{
-        return false
+    private double a;
+    private double b;
+    private double c;
+    public  Triangle (double aa,double bb,double cc){
+        a=aa;
+        b=bb;
+        c=cc;
+    }
+    public Triangle(double aa,double bb){
+        a=b=aa; 
+        c=bb;
+    }
+    public Triangle(double aa){
+        a=b=c=aa;
+    }
+    public String toString(){
+        return " a="+a+" b="+b+" c="+c;
+    }
+    public  boolean isIsoscales(){
+        if((a+b)>c&&(b+c)>a&&(c+a)>b&&a==b){
+        return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean isEquilateral(){
+        if(a==b&&b==c){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean isInequality(){
+        if((a+b)>c&&(b+c)>a&&(c+a)>b){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public boolean isEqual(Triangle t){
+        if(t.a==a&&t.b==b&&t.c==c){
+        return true;
+        }else{
+            return false;
+        }
     }
 }
-public Trinangle(double aa){
-    a=b=c=aa;
-}
-public String toString(){
-    return "a="+a+"b="+b+"c="+c;
-}
-public static boolean isIsoscales(){
-    return a==b&&
-}
-}
+
+
--- prog2_01/TriangleTester.java
+++ prog2_01/TriangleTester.java
@@ -1,5 +1,30 @@
 // 課題: 三角形クラスのテスタークラス TriangleTester.java
 
 public class TriangleTester {
-    
+    public static void main(String[] args) {
+    int n=10;
+        Triangle[] triangles = new Triangle[n];
+        triangles[0]= new Triangle(4,5,6);
+        triangles[1]= new Triangle(4,5);
+        triangles[2]= new Triangle(4);
+        triangles[3]= new Triangle(4,5,6);
+        System.out.println("三辺の長さ "+triangles[0].toString());
+        System.out.println("二等辺三角形 "+triangles[0].isIsoscales());
+        System.out.println("正三角形 " +triangles[0].isEquilateral());
+        System.out.println("三角形の成立 "+triangles[0].isInequality());
+        System.out.println("a=4,b=5,c=6の三角形と合同 "+triangles[0].isEqual(triangles[3]));
+        System.out.println();
+        System.out.println("三辺の長さ "+triangles[1].toString());
+        System.out.println("二等辺三角形 "+triangles[1].isIsoscales());
+        System.out.println("正三角形 " +triangles[1].isEquilateral());
+        System.out.println("三角形の成立 "+triangles[1].isInequality());
+        System.out.println("a=4,b=5,c=6の三角形と合同 "+triangles[1].isEqual(triangles[3]));
+        System.out.println();
+        System.out.println("三辺の長さ "+triangles[2].toString());
+        System.out.println("二等辺三角形 "+triangles[2].isIsoscales());
+        System.out.println("正三角形 " +triangles[2].isEquilateral());
+        System.out.println("三角形の成立 "+triangles[2].isInequality());
+        System.out.println("a=4,b=5,c=6の三角形と合同 "+triangles[2].isEqual(triangles[3]));
+        
+    }  
 }
