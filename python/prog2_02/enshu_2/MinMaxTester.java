83084845f178c0971cd30948a082ef1a2ae21ee5
Aroshin4 <166681444+Aroshin4@users.noreply.github.com>
2024-05-01 00:42:53 +0900

    Update MinMaxTester.java

--- python/prog2_02/enshu_2/MinMaxTester.java
+++ python/prog2_02/enshu_2/MinMaxTester.java
@@ -1,1 +1,13 @@
 // 演習2: MinMaxクラスの作成 MinMaxTester.java
+public class MinMaxTester{
+    public static void main(String[] args) {
+        MinMax a = new MinMax();
+        int n[] = {50,70,40,80,55};
+        System.out.println("二つの値の最大値"+a.Max(10,20));
+        System.out.println("三つの値の最大値"+a.Max(10,20,30));
+        System.out.println("配列の要素の最大値"+a.Max(n));
+        System.out.println("二つの値の最小値"+a.Min(10,20));
+        System.out.println("三つの値の最小値"+a.Min(10,20,30));
+        System.out.println("配列の要素の最小値"+a.Min(n));
+    }
+}
