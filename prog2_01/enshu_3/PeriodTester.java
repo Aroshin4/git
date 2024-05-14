4fdf4adb8c65964881f91e2e53395ccf3ad3663f
Aroshin4 <166681444+Aroshin4@users.noreply.github.com>
2024-05-15 02:55:22 +0900

    Delete prog2_01/enshu_3 directory

--- prog2_01/enshu_3/PeriodTester.java
+++
@@ -1,9 +0,0 @@
-// 演習問題3: 期間クラスのテスタークラス PeriodTester.java
-public class PeriodTester {
-    public static void main(String[] args) {
-
-        Period[] a = new Period[1];
-        a[0]= new Period(new Day(2000,4,20),new Day(2024,5,26));
-        System.out.println(a[0].toStr());
-}
-}
