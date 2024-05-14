fa276437060d43f6ab865c39e9504099ecbaef83
Aroshin4 <166681444+Aroshin4@users.noreply.github.com>
2024-05-15 02:44:55 +0900

    Delete python/prog2_01 directory

--- python/prog2_01/prog2_02/challenge/AlphameticValue.java
+++
@@ -1,1 +0,0 @@
-// チャレンジ課題: 覆面算 AlphameticValue.java
--- python/prog2_01/prog2_03/enshu_2/TimeAccount.java
+++
@@ -1,8 +0,0 @@
-// 演習問題2：銀行口座クラスの活用 TimeAccount.java
-class TimeAccount extends Account{
-    TimeAccount(String n, String num, long b, int year, int month, int date,long timeBalance){
-        super(n, num, timeBalance, year, month, date);
-        super.timeBalance=timeBalance;
-    }
-}
-
